75301d2bd79f8e1a2e29094776b834d9787bbfa9
Ethan Lawler <lawlerem@gmail.com>
2025-09-26 14:40:34 -0300

    Updated lcspline syntax and documentation

--- R/nnspline-package.R
+++ R/nnspline-package.R
@@ -1,6 +1,11 @@
 ## usethis namespace: start
+#' @importFrom Matrix sparseMatrix
 #' @importFrom methods as
 #' @importFrom stats dist complete.cases
+#' @importFrom stats dnorm
+#' @importFrom stats nlminb
+#' @importFrom stats qlogis
+#' @importFrom stats rnorm
 #' @importFrom utils combn
 #' @importFrom utils head tail
 ## usethis namespace: end
